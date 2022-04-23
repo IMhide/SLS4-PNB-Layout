47087dd3e79d8026cc89bbe2fe513bccbe98c3c2
Sam <sam.ettahiri@aktio.cc>
2022-04-23 19:39:47 +0200

    Fix Team images

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -7,16 +7,12 @@ const Topbar = ({infos}) => {
   const redTeamName = infos.redTeam.name;
   const [blueLogo, setBlueLogo] = useState(null) 
   const [redLogo, setRedLogo] = useState(null) 
-  useEffect( () => {
-    import(`../../logos/${blueTeamName.toUpperCase()}Logo.png`).then((logo) => setBlueLogo(logo.default))
-    import(`../../logos/${redTeamName.toUpperCase()}Logo.png`).then((logo) => setRedLogo(logo.default))
-  }, [])
 
   return(template({
     blueTeamName,
     redTeamName,
-    blueLogo,
-    redLogo,
+    blueLogo: `/logos/${blueTeamName.toUpperCase()}Logo.png`,
+    redLogo: `/logos/${redTeamName.toUpperCase()}Logo.png`,
     blueScore: infos.blueTeam.score,
     redScore: infos.redTeam.score
   }))
